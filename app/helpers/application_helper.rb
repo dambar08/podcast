a130dbf0b8f64101a02825ed70b0445ce18f5ba7
dambar08 <notdambar@gmail.com>
2025-09-17 01:07:21 +0800

    add: global helpers for views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,4 +25,25 @@ module ApplicationHelper
 
     markdown.render(text).html_safe
   end
+
+  def app_url(uri = nil)
+    base_url = "http://localhost" #TODO Change to support production
+    base_url += ":3000" if Rails.env.development? && !base_url.include?(":3000")
+    return base_url unless uri
+    Addressable::URI.parse(base_url).join(uri).normalize.to_s
+  end
+
+  def meta_keywords_default
+    return if Setting.meta_keywords[:default].blank?
+
+    tag.meta name: "keywords", content: Setting.meta_keywords[:default]
+  end
+
+  def community_name
+    "My Podcast"
+  end
+
+  def community_description
+    "What is this podcast about?"
+  end
 end
