6fec078df9fec3fd1dc2dc42259a34820df33936
dambar08 <notdambar@gmail.com>
2025-09-17 01:06:12 +0800

    add: general setting default

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -10,4 +10,31 @@ class Setting < RailsSettings::Base
   # field :admin_emails, default: "admin@rubyonrails.org", type: :array
   # field :omniauth_google_client_id, default: (ENV["OMNIAUTH_GOOGLE_CLIENT_ID"] || ""), type: :string, readonly: true
   # field :omniauth_google_client_secret, default: (ENV["OMNIAUTH_GOOGLE_CLIENT_SECRET"] || ""), type: :string, readonly: true
+  scope :general do
+    field :meta_keywords, type: :hash, default: {
+      default: nil,
+      article: nil,
+      tag: nil
+    }
+
+    field :main_social_image,
+      type: :string,
+      default: proc {
+        host ||= ActionController::Base.asset_host || "http://localhost:3000"
+        ActionController::Base.helpers.image_url("social-media-cover.png", host: host)
+       },
+      validates: { url: true }
+
+    field :social_media_handles, type: :hash, default: {
+      twitter: nil,
+      facebook: nil,
+      github: nil,
+      instagram: nil,
+      twitch: nil,
+      mastodon: nil,
+      youtube: nil,
+      linkedin: nil,
+      bluesky: nil
+    }
+  end
 end
