ef4c0736013fa2fd8b28fea860b256b8b682e962
dambar08 <notdambar@gmail.com>
2025-09-22 14:42:38 +0800

    add: help page (home)

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -2,6 +2,10 @@ class HelpsController < ApplicationController
   allow_unauthenticated_access
 
   def show
-    @pagy, @articles = pagy(Article.published.for_help)
+    parent = Category.find_by(name: "help")
+    @categories = Category.where(parent: parent) if parent.present?
+    @categories ||= Category.none
+    @categories = @categories.decorate
+    @articles = Article.published.for_help.order("RANDOM()").take(5)
   end
 end
---
+++ app/decorators/category_decorator.rb
@@ -0,0 +1,20 @@
+class CategoryDecorator < Draper::Decorator
+  delegate_all
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  def created_at
+    helpers.content_tag :span, class: 'time' do
+      object.created_at.strftime("%a %m/%d/%y")
+    end
+  end
+
+  def title
+    I18n.t("decorators.category.#{object.name}.title")
+  end
+
+  def description
+    I18n.t("decorators.category.#{object.name}.description")
+  end
+end
