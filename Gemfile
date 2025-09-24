eb393c5dae327d75b505c3cfb0ee01da1bde1983
dambar08 <notdambar@gmail.com>
2025-09-24 21:07:04 +0800

    replace importmap and tailwindcss-rails with jsbundling-rails and cssbundling-rails

--- Gemfile
+++ Gemfile
@@ -5,10 +5,10 @@ gem "rails", "~> 8.0.2" # Bundle edge Rails instead: gem "rails", github: "rails
 gem "propshaft" # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
 gem "pg", "~> 1.1" # Use postgresql as the database for Active Record
 gem "puma", ">= 5.0" # Use the Puma web server [https://github.com/puma/puma]
-gem "importmap-rails" # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
+gem "jsbundling-rails"
+gem "cssbundling-rails"
 gem "turbo-rails" # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem "stimulus-rails" # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
-gem "tailwindcss-rails" # Use Tailwind CSS [https://github.com/rails/tailwindcss-rails]
 gem "jbuilder" # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "bcrypt", "~> 3.1.7" # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 gem "tzinfo-data", platforms: %i[ windows jruby ] # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,68 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from '@hotwired/stimulus'
+import Notification from '@stimulus-components/notification'
+import Dropdown from '@stimulus-components/dropdown'
+import Dialog from '@stimulus-components/dialog'
+import ScrollTo from '@stimulus-components/scroll-to'
+import ContentLoader from '@stimulus-components/content-loader'
+import Prefetch from '@stimulus-components/prefetch'
+import AutoSubmit from '@stimulus-components/auto-submit'
+import ScrollReveal from '@stimulus-components/scroll-reveal'
+import RevealController from '@stimulus-components/reveal'
+import AnimatedNumber from '@stimulus-components/animated-number';
+import Carousel from '@stimulus-components/carousel';
+import CharacterCounter from '@stimulus-components/character-counter';
+import Chartjs from '@stimulus-components/chartjs';
+import CheckboxSelectAll from '@stimulus-components/checkbox-select-all';
+import Clipboard from '@stimulus-components/clipboard';
+import ColorPicker from '@stimulus-components/color-picker';
+import Lightbox from '@stimulus-components/lightbox';
+import PasswordVisibility from '@stimulus-components/password-visibility';
+import PlacesAutocomplete from 'stimulus-places-autocomplete';
+import Popover from '@stimulus-components/popover';
+import RailsNestedForm from '@stimulus-components/rails-nested-form';
+import ReadMore from '@stimulus-components/read-more';
+import RemoteRails from '@stimulus-components/remote-rails';
+import ScrollProgress from '@stimulus-components/scroll-progress';
+import Sortable from '@stimulus-components/sortable';
+import TextareaAutogrow from 'stimulus-textarea-autogrow';
+import Timeago from '@stimulus-components/timeago';
+import Glow from 'stimulus-glow';
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+application.register('notification', Notification);
+application.register('dropdown', Dropdown);
+application.register('dialog', Dialog);
+application.register('scroll-to', ScrollTo);
+application.register('content-loader', ContentLoader);
+application.register('prefetch', Prefetch);
+application.register('auto-submit', AutoSubmit);
+application.register('scroll-reveal', ScrollReveal);
+application.register('character-counter', CharacterCounter);
+application.register('animated-number', AnimatedNumber);
+application.register('carousel', Carousel);
+application.register('character-counter', CharacterCounter);
+application.register('chartjs', Chartjs);
+application.register('checkbox-select-all', CheckboxSelectAll);
+application.register('clipboard', Clipboard);
+application.register('color-picker', ColorPicker);
+application.register('lightbox', Lightbox);
+application.register('reveal', RevealController)
+application.register('password-visibility', PasswordVisibility);
+application.register('places-autocomplete', PlacesAutocomplete);
+application.register('popover', Popover);
+application.register('nested-form', RailsNestedForm);
+application.register('read-more', ReadMore);
+application.register('remote-rails', RemoteRails);
+application.register('reveal-controller', RevealController);
+application.register('scroll-progress', ScrollProgress);
+application.register('sortable', Sortable);
+application.register('textarea-autogrow', TextareaAutogrow);
+application.register('timeago', Timeago);
+application.register('glow', Glow);
 
 export { application }
