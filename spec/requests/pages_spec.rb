cbfbe23d079c1fb7089597239acddabff06135c7
dambar08 <notdambar@gmail.com>
2025-09-28 18:40:45 +0800

    add controller testing

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /new" do
+    it "returns a successful response" do
+      get "/new"
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET /mobile" do
+    it "returns a successful response" do
+      get "/mobile"
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
